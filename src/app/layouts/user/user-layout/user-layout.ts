c080845638a16cb2d161a825ab4b3f300ed17604
ahmed abdelnasser <ahmed.abdelnasser@everlasteg.local>
2026-04-22 18:27:32 +0200

    created userlayout

--- src/app/layouts/user/user-layout/user-layout.ts
+++ src/app/layouts/user/user-layout/user-layout.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { FolderCode, GraduationCap, LucideAngularModule, Settings } from "lucide-angular";
 
 @Component({
   selector: 'app-user-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LucideAngularModule, RouterLinkWithHref],
   templateUrl: './user-layout.html',
   styleUrl: './user-layout.scss',
 })
 export class UserLayout {
 
+  readonly folderCode = FolderCode;
+  readonly GraduationCap = GraduationCap;
+  readonly Settings = Settings;
 }
