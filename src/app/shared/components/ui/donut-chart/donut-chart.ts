66410f42f34f53f9e7ec00ae180e98f9839e6912
ahmed abdelnasser <ahmed.abdelnasser@everlasteg.local>
2026-04-28 19:03:56 +0300

    small update

--- src/app/shared/components/ui/donut-chart/donut-chart.ts
+++ src/app/shared/components/ui/donut-chart/donut-chart.ts
@@ -31,32 +31,34 @@ export class DonutChart implements AfterViewInit, OnChanges {
   }
 
   renderChart() {
-    if(!isPlatformBrowser(this.platformID)) return;
-    if (!this.canvas) return;
+    if (!isPlatformBrowser(this.platformID)) return;
+      if (!this.canvas) return;
 
-    const ctx = this.canvas.nativeElement.getContext('2d');
-    if (!ctx) return;
+      const ctx = this.canvas.nativeElement.getContext('2d');
+      if (!ctx) return;
 
-    if (this.chart) {
-      this.chart.destroy();
-    }
-
-    this.chart = new Chart(ctx, {
-      type: 'doughnut',
-      data: {
-        labels: ['Wrong', 'Correct'],
-        datasets: [{
-          data: [this.incorrect, this.correct],
-          backgroundColor: ['#ef4444', '#22c55e'],
-          borderWidth: 0
-        }]
-      },
-      options: {
-        cutout: '70%',
-        plugins: {
-          legend: { display: false }
-        }
+      if (this.chart) {
+        this.chart.destroy();
       }
-    });
+
+      this.chart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          labels: ['Wrong', 'Correct'],
+          datasets: [{
+            data: [this.incorrect, this.correct],
+            backgroundColor: ['#ef4444', '#22c55e'],
+            borderWidth: 0
+          }]
+        },
+        options: {
+          cutout: '70%',
+          plugins: {
+            legend: { display: false }
+          }
+        }
+      });
+    
   }
+
 }
