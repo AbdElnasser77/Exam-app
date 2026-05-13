2b6d84df4f44241646cb9b82370c91e93e7dbb39
AbdElnasser77 <a7med3bnaser77@gmail.com>
2026-05-14 00:21:46 +0300

    adjusted server rendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,9 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'diplomas/:diplomaId/exams', renderMode: RenderMode.Server },
+  { path: 'diplomas/:diplomaId/exams/:examId', renderMode: RenderMode.Server },
+  { path: 'diplomas/:diplomaId/exams/:examId/results/:submissionId', renderMode: RenderMode.Server },
   {
     path: '**',
     renderMode: RenderMode.Prerender
